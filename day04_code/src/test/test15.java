bfe2fa4d3e6be47ee28ae57290cb01d7260b4cbc
Gee584793330 <Gee584793330@live.com>
2022-11-22 18:02:32 +0800

    回文数练习
    Signed-off-by: Gee584793330 <Gee584793330@live.com>

--- day04_code/src/test/test15.java
+++ day04_code/src/test/test15.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.Scanner;
+
 public class test15 {
     public static void main(String[] args) {
 /*        案例2: 回文数
@@ -10,6 +12,25 @@ public class test15 {
         */
 //        数字进行倒过来，然后进行比较
 
+//        定义值：
+        Scanner sc = new Scanner(System.in);
+        System.out.print("请输入数字：");
+        int origin = sc.nextInt();
+//        定义值存放转换后的值；
+        int num=origin;
+        int result = 0;
+        while (num != 0) {
+            int last = num % 10;
+//          num进行左移一位；
+            num = num / 10;
+            result = result * 10 + last;
+        }
+        System.out.println("转换后数字为："+result);
+        if (result==origin){
+            System.out.println("该数字为回文数");
+        }else {
+            System.out.println("该数字不为回文数");
+        }
 
     }
 }
