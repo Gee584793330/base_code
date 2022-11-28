2aa6b04ce963fbcf6f7e952cefd60ecd4270ac44
Gee584793330 <Gee584793330@live.com>
2022-11-28 11:47:07 +0800

    平方根获取实例
    Signed-off-by: Gee584793330 <Gee584793330@live.com>

--- day5/src/com/gee/test/test2.java
+++ day5/src/com/gee/test/test2.java
@@ -1,8 +1,24 @@
 package com.gee.test;
 
+import java.util.Scanner;
+
 public class test2 {
     public static void main(String[] args) {
-        /*需求:键盘录入一个大于等于2的整数 x ，计算并返回 x 的 平方根结果只保留整数部分 ，小数部分将被舍去 。*/
+        /*需求:键盘录入一个大于等于2的整数 x ，计算并返回 x 的平方根结果只保留整数部分 ，小数部分将被舍去 。*
+         */
+        Scanner sc=new Scanner(System.in);
+        System.out.print("输入一个数：");
+        int num=sc.nextInt();
 
+        for (int i=1;i<=num;i++){
+            if (i*i==num){
+                System.out.println("平方根为："+i);
+                break;
+            }else if (i*i>num){
+                System.out.println("平方根整数部分为："+(i-1));
+                break;
+            }
+
+        }
     }
 }
